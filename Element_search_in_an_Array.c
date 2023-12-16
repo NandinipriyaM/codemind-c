1e50f443a31da00a89245b02272a465d4b1af485
NandinipriyaM <147844106+NandinipriyaM@users.noreply.github.com>
2023-12-16 10:35:52 +0530

    #include<stdio.h> int main() { 	int n,flag=0; 	scanf("%d",&n); 	int i,arr[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int se; 	scanf("%d",&se); 	for(i=0;i<n;i++) 	{ 		if(se==arr[i]) 		{ 			flag=1; 			break; 		} 	} 	if(flag==1) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	}
    }

---
+++ Element_search_in_an_Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int n,flag=0;
+	scanf("%d",&n);
+	int i,arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int se;
+	scanf("%d",&se);
+	for(i=0;i<n;i++)
+	{
+		if(se==arr[i])
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+	
+}
